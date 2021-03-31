9d6f5f85b41ffcf0451c48dba0fcc3e5f06c0ca8
Adam <i@adamurban.de>
2021-03-31 14:49:19 +0200

    chore(angular): update version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,47 +8,72 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core'
 import { TranslateHttpLoader } from '@ngx-translate/http-loader'
-import { ClickOutsideModule } from 'ng-click-outside'
-import { HighlightModule } from 'ngx-highlightjs'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { AboutComponent } from './components/about/about.component'
+import { ArtItemComponent } from './components/art/art-item/art-item.component'
+import { ArtComponent } from './components/art/art.component'
+import { ArticleComponent } from './components/article/article.component'
+import { ArticlesComponent } from './components/articles/articles.component'
+import { BookComponent } from './components/books/book/book.component'
+import { BooksComponent } from './components/books/books.component'
+import { ContactComponent } from './components/contact/contact.component'
+import { HeaderMenuItemComponent } from './components/header-menu-item/header-menu-item.component'
+import { HeaderComponent } from './components/header/header.component'
 import { HomeComponent } from './components/home/home.component'
-import { LoadingComponent } from './components/loading/loading.component';
-import { HeaderComponent } from './components/header/header.component';
-import { SocialIconComponent } from './components/social-icon/social-icon.component';
-import { SocialComponent } from './components/social/social.component';
-import { ArticleComponent } from './components/article/article.component';
-import { ArticlesComponent } from './components/articles/articles.component';
-import { IntroComponent } from './components/intro/intro.component';
-import { JobComponent } from './components/job/job.component';
-import { JobsComponent } from './components/jobs/jobs.component';
-import { HeaderMenuItemComponent } from './components/header-menu-item/header-menu-item.component';
-import { JobCardComponent } from './components/job/job-card/job-card.component';
-import { JobDescriptionComponent } from './components/job/job-description/job-description.component';
-import { TechTagComponent } from './components/job/tech-tag/tech-tag.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { SideprojectComponent } from './components/sideproject/sideproject.component';
-import { SideprojectsComponent } from './components/sideprojects/sideprojects.component';
-import { NewsComponent } from './components/news/news.component';
-import { NewsItemComponent } from './components/news/news-item/news-item.component';
-import { UsesComponent } from './components/uses/uses.component';
-import { BooksComponent } from './components/books/books.component';
-import { BookComponent } from './components/books/book/book.component';
-import { AboutComponent } from './components/about/about.component';
-import { ArtComponent } from './components/art/art.component';
-import { ArtItemComponent } from './components/art/art-item/art-item.component';
+import { IntroComponent } from './components/intro/intro.component'
+import { JobCardComponent } from './components/job/job-card/job-card.component'
+import { JobDescriptionComponent } from './components/job/job-description/job-description.component'
+import { JobComponent } from './components/job/job.component'
+import { TechTagComponent } from './components/job/tech-tag/tech-tag.component'
+import { JobsComponent } from './components/jobs/jobs.component'
+import { LoadingComponent } from './components/loading/loading.component'
+import { NewsItemComponent } from './components/news/news-item/news-item.component'
+import { NewsComponent } from './components/news/news.component'
 import { SideprojectDetailsComponent } from './components/sideproject/sideproject-details/sideproject-details.component'
+import { SideprojectComponent } from './components/sideproject/sideproject.component'
+import { SideprojectsComponent } from './components/sideprojects/sideprojects.component'
+import { SocialIconComponent } from './components/social-icon/social-icon.component'
+import { SocialComponent } from './components/social/social.component'
+import { UsesComponent } from './components/uses/uses.component'
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json')
 }
 
 @NgModule({
-  declarations: [AppComponent, LoadingComponent, HomeComponent, HeaderComponent, SocialIconComponent, SocialComponent, ArticleComponent, ArticlesComponent, IntroComponent, JobComponent, JobsComponent, HeaderMenuItemComponent, JobCardComponent, JobDescriptionComponent, TechTagComponent, ContactComponent, SideprojectComponent, SideprojectsComponent, NewsComponent, NewsItemComponent, UsesComponent, BooksComponent, BookComponent, AboutComponent, ArtComponent, ArtItemComponent, SideprojectDetailsComponent],
+  declarations: [
+    AppComponent,
+    LoadingComponent,
+    HomeComponent,
+    HeaderComponent,
+    SocialIconComponent,
+    SocialComponent,
+    ArticleComponent,
+    ArticlesComponent,
+    IntroComponent,
+    JobComponent,
+    JobsComponent,
+    HeaderMenuItemComponent,
+    JobCardComponent,
+    JobDescriptionComponent,
+    TechTagComponent,
+    ContactComponent,
+    SideprojectComponent,
+    SideprojectsComponent,
+    NewsComponent,
+    NewsItemComponent,
+    UsesComponent,
+    BooksComponent,
+    BookComponent,
+    AboutComponent,
+    ArtComponent,
+    ArtItemComponent,
+    SideprojectDetailsComponent,
+  ],
   imports: [
     CommonModule,
     FormsModule,
-    HighlightModule,
     HttpClientModule,
     BrowserModule,
     BrowserAnimationsModule,
@@ -57,7 +82,6 @@ export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
     ScrollingModule,
     AppRoutingModule,
     FontAwesomeModule,
-    ClickOutsideModule,
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
